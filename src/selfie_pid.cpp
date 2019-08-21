91d1692d51d8e00a8b9f657726e457a8a3ee491f
sewoongmin <gosupia@naver.com>
2019-08-21 22:00:52 +0900

    bug fix

--- src/selfie_pid.cpp
+++ src/selfie_pid.cpp
@@ -6,18 +6,15 @@
 #include <selfie_drone/MsgState.h>
 
 geometry_msgs::PoseStamped pose;
-double current_x, current_y, current_z;
+selfie_drone::MsgState centroid;
 
 void poseCB(const geometry_msgs::PoseStamped::ConstPtr &msg){
     pose = *msg;
 }
 
 void errCB(const selfie_drone::MsgState::ConstPtr &msg){
-    current_x = msg->box_size;
-    current_y = msg->x_mid;
-    current_z = msg->y_mid;
+    centroid = *msg;
 }
-
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "selfie_pid");
@@ -28,45 +25,40 @@ int main(int argc, char **argv){
     ros::ServiceClient goto_client = nh.serviceClient<swarm_ctrl_pkg::srvMultiSetpointLocal>("/multi_setpoint_local");
     ros::Rate rate(10);
 
-    PIDController<double> pid_x, pid_y, pid_z;
-    pid_x.setDt(0.1);
+    PIDController<tf2::Vector3> pid;
+    pid.setDt(0.1);
 
     int cnt =0;
-    double target_x, target_y, target_z;
     double control_x, control_y, control_z;
-    target_x = 0.07;
-    target_y = 0.5;
-    target_z = 0.618;
 
     while( ros::ok() ){
         double kp, ki, kd;
-        nh.getParam("pid/kp", kp);
-        nh.getParam("pid/ki", ki);
-        nh.getParam("pid/kd", kd);
-        pid_x.setKp(kp);    
-        pid_y.setKi(ki);    
-        pid_z.setKd(kd);    
+        tf2::Vector3 control;
+        nh.getParam("/selfie_pid/pid/kp", kp);
+        nh.getParam("/selfie_pid/pid/ki", ki);
+        nh.getParam("/selfie_pid/pid/kd", kd);
+        pid.setKp(kp);
 
-        control_x = pid_x.calculate(target_x, current_x);
-        control_y = pid_y.calculate(target_y, current_y);
-        control_z = pid_z.calculate(target_z, current_z);
+        control = pid.calculate(tf2::Vector3(0.07, 0.5, 0.618), 
+            tf2::Vector3(centroid.box_size, centroid.x_mid, centroid.y_mid));
 
         swarm_ctrl_pkg::srvMultiSetpointLocal msg;
         msg.request.formation = "POINT";
-        msg.request.x = pose.pose.position.x + control_x;
-        msg.request.y = pose.pose.position.y + control_y;
-        msg.request.z = pose.pose.position.z + control_z;
+        msg.request.x = pose.pose.position.x + control.getX();
+        msg.request.y = pose.pose.position.y + control.getY();
+        msg.request.z = pose.pose.position.z + control.getZ();
         
         if(cnt%10 == 0){
-            ROS_INFO_STREAM("control_x : " << control_x);
-            ROS_INFO_STREAM("control_y : " << control_y);
-            ROS_INFO_STREAM("control_z : " << control_z);
+            ROS_INFO_STREAM("control_x : " << control.getX());
+            ROS_INFO_STREAM("control_y : " << control.getY());
+            ROS_INFO_STREAM("control_z : " << control.getZ());
         }
-
+        cnt++;
+        
         if (goto_client.call(msg) && msg.response.success)
             ;
         else
-            ROS_ERROR("Failed to call multi_setpoint_local service.");
+           ROS_ERROR("Failed to call multi_setpoint_local service.");
         
         ros::spinOnce();
         rate.sleep();
