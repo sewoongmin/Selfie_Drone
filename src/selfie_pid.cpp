e4598d1b2acfc55608abb889d56d942b949cdf71
sewoongmin <gosupia@naver.com>
2019-08-21 21:12:42 +0900

    pid divide

--- src/selfie_pid.cpp
+++ src/selfie_pid.cpp
@@ -6,16 +6,16 @@
 #include <selfie_drone/MsgState.h>
 
 geometry_msgs::PoseStamped pose;
-tf2::Vector3 current;
+double current_x, current_y, current_z;
 
 void poseCB(const geometry_msgs::PoseStamped::ConstPtr &msg){
     pose = *msg;
 }
 
 void errCB(const selfie_drone::MsgState::ConstPtr &msg){
-    current.setX(msg->box_size);
-    current.setY(msg->x_mid);
-    current.setZ(msg->y_mid);
+    current_x = msg->box_size;
+    current_y = msg->x_mid;
+    current_z = msg->y_mid;
 }
 
 
@@ -28,29 +28,40 @@ int main(int argc, char **argv){
     ros::ServiceClient goto_client = nh.serviceClient<swarm_ctrl_pkg::srvMultiSetpointLocal>("/multi_setpoint_local");
     ros::Rate rate(10);
 
-    PIDController<tf2::Vector3> pid;
-    pid.setDt(0.1);
+    PIDController<double> pid_x, pid_y, pid_z;
+    pid_x.setDt(0.1);
 
-    tf2::Vector3 target, control_value;
-    target.setX(0.07);
-    target.setY(0.5);
-    target.setZ(0.618);
+    int cnt =0;
+    double target_x, target_y, target_z;
+    double control_x, control_y, control_z;
+    target_x = 0.07;
+    target_y = 0.5;
+    target_z = 0.618;
 
     while( ros::ok() ){
         double kp, ki, kd;
         nh.getParam("pid/kp", kp);
         nh.getParam("pid/ki", ki);
         nh.getParam("pid/kd", kd);
-        pid.setKp(kp);    
-        pid.setKi(ki);    
-        pid.setKd(kd);    
+        pid_x.setKp(kp);    
+        pid_y.setKi(ki);    
+        pid_z.setKd(kd);    
 
-        control_value = pid.calculate(target, current);
+        control_x = pid_x.calculate(target_x, current_x);
+        control_y = pid_y.calculate(target_y, current_y);
+        control_z = pid_z.calculate(target_z, current_z);
+
         swarm_ctrl_pkg::srvMultiSetpointLocal msg;
         msg.request.formation = "POINT";
-        msg.request.x = pose.pose.position.x + control_value.getX();
-        msg.request.y = pose.pose.position.y + control_value.getY();
-        msg.request.z = pose.pose.position.z + control_value.getZ();
+        msg.request.x = pose.pose.position.x + control_x;
+        msg.request.y = pose.pose.position.y + control_y;
+        msg.request.z = pose.pose.position.z + control_z;
+        
+        if(cnt%10 == 0){
+            ROS_INFO_STREAM("control_x : " << control_x);
+            ROS_INFO_STREAM("control_y : " << control_y);
+            ROS_INFO_STREAM("control_z : " << control_z);
+        }
 
         if (goto_client.call(msg) && msg.response.success)
             ;
